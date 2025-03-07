2053066388d47ab6e39343477bda2f71aedf5776
Lokmanavsar <avsarlokman@gmail.com>
2025-03-06 22:54:19 -0500

    Fix errors and pass pre-commit hook

---
+++ infrastructure/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.2.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Azure Resource 
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "Canada Central"
+}
+
+# Storage Account 
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacct"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
